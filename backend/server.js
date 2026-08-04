099ac6803e7c9f8d1f7038084f8f8a362f22974c
PrathamJcaspIntern <internjcasptechnologies.com>
2026-08-04 17:55:05 +0530

    feat(backend-server):add backend server code with dotenv port and add gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Backend Started")
+import express from "express"
+import dotenv from "dotenv"
+
+const app = express()
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello World</h1>")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server has run on https://localhost:${PORT}`)
+})
