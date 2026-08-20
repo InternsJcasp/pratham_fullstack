bbac1c14112b7ce63bce3adca6176a36b3049abf
InternsJcasp <intern@jcasptechnologies.com>
2026-08-20 19:13:13 +0530

    update a student management

---
+++ JavaScript/studentManagement/main.js
@@ -0,0 +1,7 @@
+const studentDataStorage = localStorage.getItem("STUDENT") || [];
+
+// DOM selector
+const form = document.querySelector("#student-form");
+const studentName = document.querySelector("#student-name");
+const studentAge = document.querySelector("#student-age");
+const studentCourse = document.querySelector("")
